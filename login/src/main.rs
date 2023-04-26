e4591463595e82558e08de9b2b8a85939e80c61d
ar4toX <100316298+ar4toX@users.noreply.github.com>
2023-04-26 12:15:38 -0600

    Added main file

--- login/src/main.rs
+++ login/src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let count:i32=0;
+    enter_password(count);
+}
+
+fn enter_password(mut count: i32) {
+    let mut password = String::new();
+
+    println!("Enter the password: ");
+    io::stdin().read_line(&mut password)
+        .expect("Failed to read password");
+
+    let password = password.trim().parse::<i32>()
+        .expect("Invalid password");
+
+    if password == 1234 {
+        println!("Access granted");
+    } else {
+        if count<=4 {
+            println!("Access denied");
+            count+=1;
+            enter_password(count);
+        }
+        else {
+            println!("Too many attempts, die!");
+        }
+    }
 }
